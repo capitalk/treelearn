10e97dab116c9a9f61e209d13c25f94f82808405
Alex Rubinsteyn <alexr@cs.nyu.edu>
2011-09-10 18:56:33 -0400

    added pretty printing

--- random_forest.py
+++ random_forest.py
@@ -2,21 +2,20 @@ import numpy as np
 import random 
 import math 
 
-import tree 
-
-def _majority(classes, labels): 
-    votes = np.zeros(len(classes))
-    for i, c in enumerate(classes):
-        votes[i] = np.sum(labels == c)
-    majority_idx = np.argmax(votes)
-    return classes[majority_idx] 
+from tree_helpers import * 
+import randomized_tree as tree 
     
 class RandomForest:
-    def __init__(self, classes = None, bag_percent=0.7, numtrees = 50):
+    def __init__(self, classes = None, bag_percent=0.7, numtrees = 50, **keywords):
         self.classes = None
         self.trees = [] 
         self.bag_percent = bag_percent
         self.numtrees = numtrees 
+        self.tree_params = keywords 
+    
+    def __str__(self): 
+        treeStrings = ["tree " + str(i) + ": " + str(t) for i,t in enumerate(self.trees)]
+        return "[RandomForest]\n" + "\n".join(treeStrings)
         
     def fit(self, X,Y):
         if self.classes is None: 
@@ -29,20 +28,18 @@ class RandomForest:
         n = X.shape[0]
         bagsize = int(self.bag_percent * n)
         permute = np.random.permutation
+        
         for i in xrange(self.numtrees):
             p = permute(n)
             indices = p[:bagsize] 
             data_bag = X[indices, :]
             label_bag = Y[indices] 
             
-            t = tree.RandomizedTree(classes=classes)
+            t = tree.RandomizedTree(classes=classes, **self.tree_params)
             t.fit(data_bag,label_bag)
             self.trees.append(t)
             
     def predict(self, X):
         votes = [t.predict(X) for t in self.trees]
         return majority(self.classes, votes)
-        
-            
-            
         
--- test_random_forest.py
+++ test_random_forest.py
@@ -1,16 +1,14 @@
 
 import numpy as np 
-import rf
+import random_forest as rf 
 
-def test_majority():
-    x = np.array([0,0,0,1,1,0,2,2,2,2,2,0,1,0])
-    assert rf.majority([0,1,2], x) == 0
 
 def test_simple_tree():
     data = np.array([[0,0], [0.1, 0.1], [0.15, 0.15], [0.98, 0.98], [1.0, 1.0], [.99,.99]])
     labels = np.array([0,0,0, 1,1,1])
-    forest = rf.RandomForest()
+    forest = rf.RandomForest(min_leaf_size=1)
     forest.fit(data,labels)
+    print forest 
     pred0 = forest.predict(np.array([0.05, 0.05]))
     print "Expected: 0, Received:", pred0
     assert pred0 == 0
