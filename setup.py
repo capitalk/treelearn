cbd1ff94ed0bffddbfeb366493a3321fb55b782e
Alex Rubinsteyn <alexr@cs.nyu.edu>
2011-11-02 16:03:54 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
    name = "treelearn",
-   version = "0.0.6",
+   version = "0.0.7",
    #package_dir = { '' : 'treelearn' },
    packages = ['treelearn'],
    install_requires = [ 'scikit-learn' ],
--- treelearn/__init__.py
+++ treelearn/__init__.py
@@ -2,13 +2,13 @@
 from constant_leaf import ConstantLeaf
 from tree_node import TreeNode
 from randomized_tree import RandomizedTree
-from svm_tree import SVM_Tree
+from oblique_tree import ObliqueTree
 from classifier_ensemble import ClassifierEnsemble
 from recipes import train_svm_forest, train_random_forest
 
 __all__ = [
   'ClassifierEnsemble', 'RegressionEnsemble', 
   'RandomizedTree', 'TreeNode', 'ConstantLeaf', 
-  'ClassifierTree',
+  'ObliqueTree',
   'train_svm_forest', 'train_random_forest'
 ] 
