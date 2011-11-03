a4d100f90812e88c0d69498df1511bffa68bcf37
Alex Rubinsteyn <alexr@cs.nyu.edu>
2011-11-03 04:14:43 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
    name = "treelearn",
-   version = "0.0.7",
+   version = "0.0.8",
    #package_dir = { '' : 'treelearn' },
    packages = ['treelearn'],
    install_requires = [ 'scikit-learn' ],
